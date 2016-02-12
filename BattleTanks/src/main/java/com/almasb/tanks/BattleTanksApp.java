bdbaac24c20fc10b583cfbf557f863c1b3d77c7e
AlmasB <almaslvl@gmail.com>
2016-02-12 22:39:37 +0000

    update

---
+++ BattleTanks/src/main/java/com/almasb/tanks/BattleTanksApp.java
@@ -0,0 +1,163 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015-2016 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.almasb.tanks;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.EntityView;
+import com.almasb.fxgl.entity.GameEntity;
+import com.almasb.fxgl.entity.component.MainViewComponent;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.parser.TextLevelParser;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.geometry.BoundingBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
+/**
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ */
+public class BattleTanksApp extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setTitle("BattleTanks");
+        settings.setVersion("0.2-SNAPSHOT");
+        settings.setWidth(840);
+        settings.setHeight(840);
+        settings.setMenuEnabled(false);
+        settings.setIntroEnabled(false);
+    }
+
+    private PlayerControl playerControl;
+
+    @Override
+    protected void initInput() {
+        Input input = getInput();
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                playerControl.left();
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                playerControl.right();
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                playerControl.up();
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                playerControl.down();
+            }
+        }, KeyCode.S);
+
+        input.addAction(new UserAction("Shoot") {
+            @Override
+            protected void onActionBegin() {
+                playerControl.shoot();
+            }
+        }, KeyCode.F);
+    }
+
+    @Override
+    protected void initAssets() {
+
+    }
+
+    @Override
+    protected void initGame() {
+        MainViewComponent.turnOnDebugBBox(Color.RED);
+
+
+
+
+
+
+
+        TextLevelParser levelParser = new TextLevelParser();
+        levelParser.setEmptyChar('0');
+
+        levelParser.addEntityProducer('1', (x, y) -> {
+            GameEntity wall = new GameEntity();
+            wall.getPositionComponent().setValue(x * 84, y * 84);
+            wall.getMainViewComponent().setView(new EntityView(getAssetLoader().loadTexture("wall.png")), true);
+
+            return wall;
+        });
+
+        levelParser.addEntityProducer('F', (x, y) -> {
+            GameEntity flag = new GameEntity();
+            flag.getPositionComponent().setValue(x * 84, y * 84);
+            flag.getMainViewComponent().setView(new EntityView(getAssetLoader().loadTexture("flag.png")), true);
+
+            return flag;
+        });
+
+        try {
+            TextLevelParser.Level level = levelParser.parse("levels/level0.txt");
+            level.getEntities().forEach(getGameWorld()::addEntity);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load level data: " + e.getMessage());
+        }
+
+        playerControl = new PlayerControl();
+
+        GameEntity player = new GameEntity();
+        player.getBoundingBoxComponent().addHitBox(new HitBox("BODY", new BoundingBox(10, 10, 54, 54)));
+        player.addControl(playerControl);
+
+        getGameWorld().addEntity(player);
+    }
+
+    @Override
+    protected void initPhysics() {
+
+    }
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void onUpdate() {
+
+    }
+}
---
+++ BattleTanks/src/main/java/com/almasb/tanks/PlayerControl.java
@@ -0,0 +1,132 @@
+/*
+ * The MIT License (MIT)
+ *
+ * FXGL - JavaFX Game Library
+ *
+ * Copyright (c) 2015-2016 AlmasB (almaslvl@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.almasb.tanks;
+
+import com.almasb.ents.AbstractControl;
+import com.almasb.ents.Entity;
+import com.almasb.ents.component.Required;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.ServiceType;
+import com.almasb.fxgl.asset.Texture;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.component.BoundingBoxComponent;
+import com.almasb.fxgl.entity.component.MainViewComponent;
+import com.almasb.fxgl.entity.component.PositionComponent;
+import com.almasb.fxgl.entity.component.RotationComponent;
+import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
+
+/**
+ * @author Almas Baimagambetov (AlmasB) (almaslvl@gmail.com)
+ */
+@Required(PositionComponent.class)
+@Required(RotationComponent.class)
+public class PlayerControl extends AbstractControl {
+
+    private PositionComponent position;
+    private RotationComponent rotation;
+    private BoundingBoxComponent bbox;
+    private MainViewComponent view;
+
+    private Texture texture;
+
+    @Override
+    public void onAdded(Entity entity) {
+        position = Entities.getPosition(entity);
+        rotation = Entities.getRotation(entity);
+        view = Entities.getMainView(entity);
+        bbox = Entities.getBBox(entity);
+
+        texture = GameApplication.getService(ServiceType.ASSET_LOADER).loadTexture("player.png");
+        view.setGraphics(texture);
+    }
+
+    private double speed = 0;
+    private int frames = 0;
+
+    @Override
+    public void onUpdate(Entity entity, double tpf) {
+        speed = tpf * 60;
+
+        int frame = frames / 10;
+        if (frame >= 8) {
+            frame = 0;
+            frames = 0;
+        }
+
+        texture.setViewport(new Rectangle2D(frame * 84, 0, 84, 84));
+    }
+
+    public void up() {
+        rotation.setValue(270);
+        position.translateY(-5 * speed);
+        frames++;
+    }
+
+    public void down() {
+        rotation.setValue(90);
+        position.translateY(5 * speed);
+        frames++;
+    }
+
+    public void left() {
+        rotation.setValue(180);
+        position.translateX(-5 * speed);
+        frames++;
+    }
+
+    public void right() {
+        rotation.setValue(0);
+        position.translateX(5 * speed);
+        frames++;
+    }
+
+    public void shoot() {
+        Entity entity = EntityFactory.newBullet(
+                position.getX(),
+                position.getY(),
+                angleToVector());
+
+        //System.out.println(bbox.getCenterWorld());
+        System.out.println(Entities.getBBox(entity).getWidth() + " " + Entities.getBBox(entity).getHeight());
+
+        getEntity().getWorld().addEntity(entity);
+    }
+
+    private Point2D angleToVector() {
+        double angle = rotation.getValue();
+        if (angle == 0) {
+            return new Point2D(1, 0);
+        } else if (angle == 90) {
+            return new Point2D(0, 1);
+        } else if (angle == 180) {
+            return new Point2D(-1, 0);
+        } else {    // 270
+            return new Point2D(0, -1);
+        }
+    }
+}
