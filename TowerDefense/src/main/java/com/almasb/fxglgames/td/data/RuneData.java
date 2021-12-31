67230a9df7aeaddcf48b220dccbc6d314849f0cc
Almas Baimagambetov <almaslvl@gmail.com>
2021-12-31 09:51:40 +0000

    Added rune data
    RuneData objects define runes aka buffs that can be active during gameplay

---
+++ TowerDefense/src/main/java/com/almasb/fxglgames/td/data/RuneData.java
@@ -0,0 +1,11 @@
+package com.almasb.fxglgames.td.data;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public record RuneData(
+        String name,
+        int cost,
+        String description
+
+) { }
