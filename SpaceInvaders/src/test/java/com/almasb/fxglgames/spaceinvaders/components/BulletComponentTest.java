fa9e9667d7253ec59b652efddbe98c55e973d30d
Cong Ho <hovietcong2000@gmail.com>
2025-04-26 14:09:26 -0700

    generated unit test for BulletComponent

---
+++ SpaceInvaders/src/test/java/com/almasb/fxglgames/spaceinvaders/components/BulletComponentTest.java
@@ -0,0 +1,18 @@
+package com.almasb.fxglgames.spaceinvaders.components;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BulletComponentTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onUpdate() {
+    }
+}
