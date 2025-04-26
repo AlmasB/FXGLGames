32067c1b1bc496fc2560361a2376b8bd10f04b1c
tpranoto <{ID}+{username}@users.noreply.github.com>
2025-04-26 13:42:07 -0700

    add example of refactoring

---
+++ SpaceInvaders/src/test/java/com/almasb/fxglgames/spaceinvaders/components/AutoRotationComponentTest.java
@@ -0,0 +1,26 @@
+package com.almasb.fxglgames.spaceinvaders.components;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutoRotationComponentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void onAdded() {
+    }
+
+    @Test
+    void onUpdate() {
+    }
+}
---
+++ SpaceInvaders/src/test/java/com/almasb/fxglgames/spaceinvaders/components/BossComponentTest.java
@@ -0,0 +1,18 @@
+package com.almasb.fxglgames.spaceinvaders.components;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BossComponentTest {
+
+    @org.junit.jupiter.api.Test
+    void onUpdate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void shoot() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void die() {
+    }
+}
