493e69adc5b78c0326627af9a132ca2879ed48ff
Cong Ho <hovietcong2000@gmail.com>
2025-04-26 17:27:34 -0700

    added unit tests for Bullet Component

--- SpaceInvaders/src/test/java/com/almasb/fxglgames/spaceinvaders/components/BulletComponentTest.java
+++ SpaceInvaders/src/test/java/com/almasb/fxglgames/spaceinvaders/components/BulletComponentTest.java
@@ -1,18 +1,52 @@
 package com.almasb.fxglgames.spaceinvaders.components;
 
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxglgames.spaceinvaders.SpaceInvadersType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class BulletComponentTest {
-
-    @org.junit.jupiter.api.BeforeEach
+    private BulletComponent bulletComponent;
+    private OwnerComponent ownerMock;
+    private Entity entityMock;
+    @BeforeEach
     void setUp() {
+        ownerMock = mock(OwnerComponent.class);
+        entityMock = mock(Entity.class);
+        bulletComponent = new BulletComponent(10.5, ownerMock, entityMock);
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+    @ParameterizedTest(name = "{index} => tpf={0}, expected={1}")
+    @CsvSource({
+            "0.016, -0.168",
+            "0, -0",
+            "-1.1, 11.55"
+    })
+    void onUpdate_translateY_whenPlayer(double tpf, double expected) {
+        when(ownerMock.getValue()).thenReturn(SpaceInvadersType.PLAYER);
+
+        bulletComponent.onUpdate(tpf);
+
+        verify(ownerMock, times(1)).getValue();
+        verify(entityMock, times(1)).translateY(expected);
     }
 
-    @org.junit.jupiter.api.Test
-    void onUpdate() {
+    @ParameterizedTest(name = "{index} => tpf={0}, expected={1}")
+    @CsvSource({
+            "0.016, 0.168",
+            "0, 0",
+            "-1.1, -11.55"
+    })
+    void upDate_translateY_whenEnemy(double tpf, double expected) {
+        when(ownerMock.getValue()).thenReturn(SpaceInvadersType.ENEMY);
+
+        bulletComponent.onUpdate(tpf);
+
+        verify(ownerMock, times(1)).getValue();
+        verify(entityMock, times(1)).translateY(expected);
     }
 }
